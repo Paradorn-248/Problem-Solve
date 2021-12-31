5fd710125ce87976994a1222e8d027ff185dd2d2
Paradorn-248 <paradorn248@gmail.com>
2021-12-31 22:22:43 +0700

    commit all

--- week2 list vector stack/test.cpp
+++ week2 list vector stack/test.cpp
@@ -1,20 +1,69 @@
-// lower_bound/upper_bound example
-#include <iostream>     // std::cout
-#include <algorithm>    // std::lower_bound, std::upper_bound, std::sort
-#include <vector>       // std::vector
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int dp[125][125][2];
+char s[125][125];
 
-int main () {
-  int myints[] = {10,20,30,30,20,10,10,20};
-  std::vector<int> v(myints,myints+8);           // 10 20 30 30 20 10 10 20
+int dfs(int row, int col, int state)
+{
+  //
+  if (dp[row][col][state] != -1)
+    return dp[row][col][state];
+  if (col == 0)
+  {
+    state = 1;
+  }
+  if (col == m - 1)
+  {
+    state = 0;
+  }
+  if (s[row][col] == '.')
+    return dp[row][col][state] = dfs(row + 1, col, state);
+  if (s[row][col] == '$')
+    return dp[row][col][state] = 1;
+  if (s[row][col] == '@')
+    return dp[row][col][state] = 0;
+  if(state==0)
+    return dp[row][col][state] = dfs(row, col - 1, state);
+  if(state==1)
+    return dp[row][col][state] = dfs(row, col + 1, state);
 
-  std::sort (v.begin(), v.end());                // 10 10 10 20 20 20 30 30
+}
 
-  std::vector<int>::iterator low,up;
-  low=std::lower_bound (v.begin(), v.end(), 10); //          ^
-  up= std::upper_bound (v.begin(), v.end(), 10); //                   ^
-  std::cout << *low << '\n';
-  std::cout << "lower_bound at position " << (low- v.begin()) << '\n';
-  std::cout << "upper_bound at position " << (up - v.begin()) << '\n';
-
+int main()
+{
+  scanf("%d %d", &m, &n);
+  for (int a = 0; a < n; a++)
+  {
+    scanf("%s", s[a]);
+  }
+  int cnt = 0, ans1 = 0, ans2 = 0;
+  memset(dp, -1, sizeof(dp));
+  for (int a = 0; a < m; a++)
+  {
+    cnt += dfs(0, a, 0);
+    cnt += dfs(0, a, 1);
+  }
+  ans1 = cnt;
+  for (int a = 0; a < n - 1; a++)
+  {
+    for (int b = 0; b < m; b++)
+    {
+      if (s[a][b] == '#')
+      {
+        s[a][b] = '.';
+        cnt = 0;
+        memset(dp, -1, sizeof(dp));
+        for (int c = 0; c < m; c++)
+        {
+          cnt += dfs(0, c, 0);
+          cnt += dfs(0, c, 2);
+        }
+        ans2 = max(ans2, cnt);
+        s[a][b] = '#';
+      }
+    }
+  }
+  printf("%d %d", ans1, ans2);
   return 0;
 }
--- week4 graph/lemmo.cpp
+++ week4 graph/lemmo.cpp
@@ -1,15 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 string s[122];
-int w,h;
-int kstate[122][122][3];
+int w, h;
+int memm[122][122][2];
 
-int dfs(int row,int col,int state)
+int dfs(int row, int col, int state)
 {
-    if(s[row][col]=='.')
+    // state: 0=left, 1=right
+    // mem: -1=initial, 0=@, 1=$
+    if (memm[row][col][state] != -1)
     {
-        dfs(row+1,col,state);
+        return memm[row][col][state];
+    }
+    if (col == w - 1)
+    {
+        state = 0;
+    }
+    else if (col == 0)
+    {
+        state = 1;
+    }
+    if (s[row][col] == '.')
+    {
+        return memm[row][col][state] = dfs(row + 1, col, state);
+    }
+    if (s[row][col] == '@')
+    {
+        return memm[row][col][state] = 0;
+    }
+    if (s[row][col] == '$')
+    {
+        return memm[row][col][state] = 1;
+    }
+    if (state == 0)
+    {
+        return memm[row][col][state] = dfs(row, col - 1, state);
+    }
+    if (state == 1)
+    {
+        return memm[row][col][state] = dfs(row, col + 1, state);
     }
 }
 
@@ -17,16 +47,38 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    int count=0,ans1,ans2;
+
+    int ans1 = 0, ans2 = 0, tmp = 0;
     cin >> w >> h;
-    for(int i=0;i<h;i++)
+    for (int i = 0; i < h; i++)
     {
         cin >> s[i];
     }
-    memset(kstate,-1,sizeof(kstate));
-    for(int i=0;i<w;i++)
+    memset(memm, -1, sizeof(memm));
+    for (int i = 0; i < w; i++)
     {
-
+        ans1 += dfs(0, i, 1); // go right first
+        ans1 += dfs(0, i, 0); // go left first
     }
+    cout << ans1 << ' ';
+    for (int i = 0; i < h - 1; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if (s[i][j] == '#')
+            {
+                s[i][j] = '.';
+                memset(memm, -1, sizeof(memm));
+                for (int k = 0; k < w; k++)
+                {
+                    tmp += dfs(0, k, 1); // go right first
+                    tmp += dfs(0, k, 0); // go left first
+                }
+                s[i][j] = '#';
+                ans2 = max(tmp, ans2);
+                tmp = 0;
+            }
+        }
+    }
+    cout << ans2;
 }
